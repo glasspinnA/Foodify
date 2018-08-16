4d2bbfb42742086e924c13e348fb8038558a9199
glasspinnA <ozze-1995@hotmail.com>
2018-08-16 11:12:13 +0200

    Added comments for AdaptListener

--- app/src/main/java/com/example/oscar/finalapp/AdapterListener.java
+++ app/src/main/java/com/example/oscar/finalapp/AdapterListener.java
@@ -1,10 +1,10 @@
 package com.example.oscar.finalapp;
 
 /**
- * Interface för lyssna efter om användaren klickar på ta bort knappen
+ * Interface för lyssna efter om användaren klickar på ta bort knappen eller redigera en plats knappen
  * i listview över alla locations
  */
 public interface AdapterListener {
-    void onClick(MarkerLocation name);
+    void onClick(MarkerLocation markerLocation);
     void editMarker(int position);
 }
